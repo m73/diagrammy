355cee3a418774cc570118867dba7010e2af7992
Matthías Ragnarsson <mar17@hi.is>
2014-03-04 15:50:35 +0000

    Put up a demo diagram in the component.

--- diagrammy/Diagrammy.cs
+++ diagrammy/Diagrammy.cs
@@ -25,10 +25,24 @@ namespace diagrammy
 		protected override void RenderContents(HtmlTextWriter writer)
 		{
 			writer.Write("<div><p>Some new text.</p></div>");
-			string scriptName = "diagrammy.diagrammy.js";
+			string diaScriptName = "diagrammy.diagrammy.js";
+			string plumbScriptName = "diagrammy.jquery.jsPlumb-1.5.5.js";
+			string demoScriptName = "diagrammy.demo.js";
 			Type scriptType = typeof(Diagrammy);
 			ClientScriptManager cs = Page.ClientScript;
-			writer.Write ("<script src='"+ cs.GetWebResourceUrl (scriptType, scriptName) + "'></script>");
+			writer.Write ("<link rel='stylesheet' href='"+cs.GetWebResourceUrl(scriptType, "diagrammy.demo.css")+"'/>");
+			writer.Write("<div class='demo flowchart-demo' id='flowchart-demo'>");
+			writer.Write("<div class='window' id='flowchartWindow1'><strong>1</strong><br/><br/></div>");
+			writer.Write("<div class='window' id='flowchartWindow2'><strong>1</strong><br/><br/></div>");
+			writer.Write("<div class='window' id='flowchartWindow3'><strong>1</strong><br/><br/></div>");
+			writer.Write("<div class='window' id='flowchartWindow4'><strong>1</strong><br/><br/></div>");                    
+			writer.Write("</div>");
+			                        
+			writer.Write("<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>");
+			writer.Write("<script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js'></script>");
+			writer.Write("<script src='" + cs.GetWebResourceUrl(scriptType, plumbScriptName) + "'></script>");
+			writer.Write ("<script src='"+ cs.GetWebResourceUrl (scriptType, demoScriptName) + "'></script>");
+			writer.Write ("<script src='"+ cs.GetWebResourceUrl (scriptType, diaScriptName) + "'></script>");
 		}
 
 		protected override HtmlTextWriterTag TagKey
--- diagrammy/Properties/AssemblyInfo.cs
+++ diagrammy/Properties/AssemblyInfo.cs
@@ -13,6 +13,9 @@ using System.Web.UI;
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // Add client side files.
+[assembly: WebResource("diagrammy.demo.css", "text/css")]
+[assembly: WebResource("diagrammy.jquery.jsPlumb-1.5.5.js", "application/javascript")]
+[assembly: WebResource("diagrammy.demo.js", "application/javascript")]
 [assembly: WebResource("diagrammy.diagrammy.js", "application/javascript")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
